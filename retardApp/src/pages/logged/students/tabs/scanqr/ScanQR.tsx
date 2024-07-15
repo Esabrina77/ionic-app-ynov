7a70982ebf805c85aa01f4cd12c3a031b8e33d4e
Esabrina77 <sabrina.eloundou@ynov.com>
2024-07-15 15:04:12 +0200

    abscence_ retard updated

---
+++ retardApp/src/pages/logged/students/tabs/scanqr/ScanQR.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Header } from "../../../../../components/ui/Header";           
+
+import { TabWrappedComponent } from "../../../../../components/utils/TabWrapper";
+import HeaderRadius from '../../../../../components/HeaderRadius/HeaderRadius';
+
+import {
+    IonContent,
+    useIonViewDidEnter,
+    useIonViewWillLeave,
+    IonPage,
+    IonItem,
+    IonLabel,
+    IonText
+  } from '@ionic/react';
+
+export const ScanQR: React.FC<TabWrappedComponent> = ({ isTab }) => {
+    const [visible, setVisible] = useState(false);
+    const [currentTab, setCurrentTab] = useState<'actual' | 'cumul'>('actual');
+  
+    useIonViewDidEnter(() => {
+      setVisible(true);
+    });
+    useIonViewWillLeave(() => {
+      setVisible(false);
+    });
+    return (
+        <IonPage>
+          <Header title="Scanner QR" showLogo={false} />
+          <IonContent fullscreen>
+            <div className="scan-qr-container">
+              <h2>Scannez le code QR</h2>
+              {/* Ajout du composant de scan QR */}
+            </div>
+          </IonContent>
+        </IonPage>
+      );
+    };
