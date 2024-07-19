c9defd1068ab9cd24bf3a464e4a5b185d3c8f18a
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2024-07-19 15:59:25 +0200

    Cyril my beloved

--- retardApp/src/pages/logged/students/tabs/scanQr/ScanPage.tsx
+++ retardApp/src/pages/logged/students/tabs/scanQr/ScanPage.tsx
@@ -1,111 +1,112 @@
 import {
-    IonButton,
-    IonButtons,
-    IonContent,
-    IonHeader,
-    IonIcon,
-    IonPage,
-    IonRow,
-    IonText,
-    IonTitle,
-    IonToolbar,
-    useIonAlert,
-  } from "@ionic/react"
-  import { useEffect, useState } from "react"
-  import { BarcodeScanner } from "@capacitor-community/barcode-scanner"
-  import { scanOutline, stopCircleOutline } from "ionicons/icons"
-  import "./ScanPage.css"
-  
-  const ScanPage: React.FC = () => {
-    const [err, setErr] = useState<string>()
-  
-    const [hideBg, setHideBg] = useState(false)
-  
-    const [present] = useIonAlert()
-  
-    const startScan = async () => {
-      BarcodeScanner.hideBackground()
-      setHideBg(true)
-      // make background of WebView transparent
-  
-      const result = await BarcodeScanner.startScan() // start scanning and wait for a result
-  
-      // if the result has content
-      if (result.hasContent) {
-        stopScan()
-        present({
-          message: result.content,
-          buttons: [
-            "Cancel",
-            { text: "Ok", handler: (d) => console.log("ok pressed") },
-          ],
-          onDidDismiss: (e) => console.log("did dismiss"),
-        })
-        console.log(result.content) // log the raw scanned content
-      }
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToolbar,
+  useIonAlert,
+} from "@ionic/react";
+import { useEffect, useState } from "react";
+import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
+//import { scanOutline, stopCircleOutline } from "ionicons/icons";
+ import "./ScanPage.css";
+
+const ScanPage: React.FC = () => {
+  const [err, setErr] = useState<string>();
+  const [hideBg, setHideBg] = useState(false);
+  const [present] = useIonAlert();
+
+  const startScan = async () => {
+    console.log("Starting scan...");
+    BarcodeScanner.hideBackground();
+    setHideBg(true);
+
+    const result = await BarcodeScanner.startScan();
+
+    if (result.hasContent) {
+      stopScan();
+      present({
+        message: result.content,
+        buttons: [
+          "Cancel",
+          { text: "Ok", handler: (d) => console.log("ok pressed") },
+        ],
+        onDidDismiss: (e) => console.log("did dismiss"),
+      });
+      console.log(result.content);
+    } else {
+      console.log("No content found.");
     }
-  
-    const stopScan = () => {
-      BarcodeScanner.showBackground()
-      setHideBg(false)
-      BarcodeScanner.stopScan()
-    }
-  
-    useEffect(() => {
-      const checkPermission = async () => {
-        setErr(undefined)
-        try {
-          const status = await BarcodeScanner.checkPermission({ force: true })
-  
-          if (status.granted) {
-            return true
-          }
-  
-          return false
-        } catch (error) {
-          setErr(error as any)
+  };
+
+  const stopScan = () => {
+    console.log("Stopping scan...");
+    BarcodeScanner.showBackground();
+    setHideBg(false);
+    BarcodeScanner.stopScan();
+  };
+
+  useEffect(() => {
+    const checkPermission = async () => {
+      setErr(undefined);
+      try {
+        const status = await BarcodeScanner.checkPermission({ force: true });
+
+        if (status.granted) {
+          console.log("Permission granted.");
+          return true;
+        } else {
+          console.log("Permission not granted.");
+          return false;
         }
+      } catch (error) {
+        console.error("Error checking permission: ", error);
+        setErr(error as any);
       }
-  
-      checkPermission()
-  
-      return () => {}
-    }, [])
-  
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>QRScanner</IonTitle>
-            {hideBg && (
-              <IonButtons slot="end">
-                <IonButton onClick={stopScan} color="danger">
-                  <IonIcon icon={stopCircleOutline} slot="start" />
-                  Stop Scan
-                </IonButton>
-              </IonButtons>
+    };
+
+    checkPermission();
+
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>QRScanner</IonTitle>
+              {hideBg && (
+                <IonButtons slot="end">
+                  <IonButton onClick={stopScan} color="danger">
+                    <IonIcon  slot="start" />
+                    Stop Scan
+                  </IonButton>
+                </IonButtons>
+              )}
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className={hideBg ? "hideBg" : "ion-padding"}>
+            {err && (
+              <IonRow>
+                <IonText color="danger">{err}</IonText>
+              </IonRow>
             )}
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className={hideBg ? "hideBg" : "ion-padding"}>
-          {err && (
-            <IonRow>
-              {" "}
-              <IonText color="danger">{err}</IonText>
-            </IonRow>
-          )}
-  
-          {!!!err && hideBg && <div className="scan-box"></div>}
-          {!!!err && !!!hideBg && (
-            <IonButton className="center-button" onClick={startScan}>
-              <IonIcon icon={scanOutline} slot="start" />
-              Start Scan
-            </IonButton>
-          )}
-        </IonContent>
-      </IonPage>
-    )
-  }
-  
-  export default ScanPage
-  
+
+            {!!!err && hideBg && <div className="scan-box"></div>}
+            {!!!err && !!!hideBg && (
+              <IonButton className="center-button" onClick={startScan}>
+                <IonIcon slot="start" />
+                Start Scan
+              </IonButton>
+            )}
+          </IonContent>
+        </div>
+  );
+};
+
+export default ScanPage;
