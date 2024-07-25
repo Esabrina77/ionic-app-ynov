93211b3d2532bee44b4654dfba7d081a68b5b40f
MONFORTE Pierre <95189614+BUSTERDAY@users.noreply.github.com>
2024-07-25 16:13:58 +0200

    passage de js en tsx

--- retardApp/src/components/server/server.tsx
+++ retardApp/src/components/server/server.tsx
@@ -1,14 +1,17 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import nodemailer from 'nodemailer';
 import bodyParser from 'body-parser';
 
+// Créez une instance d'application Express
 const app = express();
 const port = 3000;
 
+// Middleware pour gérer les en-têtes CORS et le corps de la requête JSON
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 
+// Créez un transporteur Nodemailer pour envoyer des e-mails
 const transporter = nodemailer.createTransport({
     service: 'outlook',
     auth: {
@@ -17,15 +20,25 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-app.post('/send-email', (req, res) => {
+// Définissez une interface pour les données de la requête
+interface SendEmailRequest {
+    pdfData: string;
+    prenom: string;
+    nom: string;
+}
+
+// Route POST pour envoyer un e-mail
+app.post('/send-email', (req: Request<{}, {}, SendEmailRequest>, res: Response) => {
     const { pdfData, prenom, nom } = req.body;
 
     if (!pdfData || !prenom || !nom) {
         return res.status(400).json({ error: 'Missing required data' });
     }
 
+    // Nettoyez les données PDF
     const base64Data = pdfData.replace('data:application/pdf;base64,', '');
 
+    // Options pour l'e-mail
     const mailOptions = {
         from: 'pierre.monforte@ynov.com',
         to: `${prenom}.${nom}@ynov.com`,
@@ -40,6 +53,7 @@ app.post('/send-email', (req, res) => {
         ],
     };
 
+    // Envoyez l'e-mail
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error('Error sending email:', error);
@@ -47,10 +61,10 @@ app.post('/send-email', (req, res) => {
         }
         console.log('Email sent:', info.response);
         res.json({ message: 'Email sent successfully' });
-        return
     });
 });
 
+// Démarrez le serveur
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
