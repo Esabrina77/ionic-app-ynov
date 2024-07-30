a738a1605ff7764ccfc63adf004920545cf846e9
gambeynoa <113116077+NoaYnov@users.noreply.github.com>
2024-07-30 20:53:21 +0200

    Bello Vito

--- myw-boilerplate/src/app/modules/admin/scolarity/retard/retard.component.spec.ts
+++ myw-boilerplate/src/app/modules/admin/scolarity/retard/retard.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RetardComponent } from './retard.component';
 
 describe('RetardComponent', () => {
@@ -8,9 +7,9 @@ describe('RetardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RetardComponent]
+      declarations: [RetardComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RetardComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,18 @@ describe('RetardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch retards on init', () => {
+    spyOn(component, 'fetchRetards');
+    component.ngOnInit();
+    expect(component.fetchRetards).toHaveBeenCalled();
+  });
+
+  it('should handle fetch error', async () => {
+    spyOn(window, 'fetch').and.returnValue(Promise.reject('API is down'));
+    component.fetchRetards();
+    fixture.detectChanges();
+    expect(component.errorMessage).toBe('An error occurred while fetching the data.');
   });
 });
--- myw-boilerplate/src/app/modules/admin/scolarity/retard/retard.component.ts
+++ myw-boilerplate/src/app/modules/admin/scolarity/retard/retard.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-retard',
   standalone: true,
-  imports: [],
   templateUrl: './retard.component.html',
-  styleUrl: './retard.component.scss'
+  styleUrl: './retard.component.scss',
+  imports: [CommonModule]
 })
-export class RetardComponent {
 
+export class RetardComponent implements OnInit {
+  retards: any[] = [];
+  errorMessage: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.fetchRetards();
+  }
+
+  fetchRetards(): void {
+    fetch('/api/retard')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data[0].props.annee_id)
+        this.retards = data;
+      })
+      .catch(error => {
+        this.errorMessage = 'An error occurred while fetching the data.';
+        console.error('Error fetching retards:', error);
+      });
+  }
 }
