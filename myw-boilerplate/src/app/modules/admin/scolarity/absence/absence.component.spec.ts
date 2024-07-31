d9c386bceab2ce8c944ad3fb0d223cb619d2f0e8
NoaYnov <113116077+NoaYnov@users.noreply.github.com>
2024-07-31 09:30:32 +0200

    perfect

--- myw-boilerplate/src/app/modules/admin/scolarity/absence/absence.component.spec.ts
+++ myw-boilerplate/src/app/modules/admin/scolarity/absence/absence.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AbsenceComponent } from './absence.component';
 
 describe('AbsenceComponent', () => {
@@ -8,9 +7,9 @@ describe('AbsenceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AbsenceComponent]
+      declarations: [AbsenceComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AbsenceComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,18 @@ describe('AbsenceComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch absences on init', () => {
+    spyOn(component, 'fetchabsences');
+    component.ngOnInit();
+    expect(component.fetchabsences).toHaveBeenCalled();
+  });
+
+  it('should handle fetch error', async () => {
+    spyOn(window, 'fetch').and.returnValue(Promise.reject('API is down'));
+    component.fetchabsences();
+    fixture.detectChanges();
+    expect(component.errorMessage).toBe('An error occurred while fetching the data.');
   });
 });
--- myw-boilerplate/src/app/modules/admin/scolarity/absence/absence.component.ts
+++ myw-boilerplate/src/app/modules/admin/scolarity/absence/absence.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-absence',
   standalone: true,
-  imports: [],
   templateUrl: './absence.component.html',
-  styleUrl: './absence.component.scss'
+  styleUrl: './absence.component.scss',
+  imports: [CommonModule]
 })
-export class AbsenceComponent {
 
+export class AbsenceComponent implements OnInit {
+  absences: any[] = [];
+  errorMessage: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.fetchabsences();
+  }
+
+  fetchabsences(): void {
+    fetch('/api/abs')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data[0].props.annee_id)
+        this.absences = data;
+      })
+      .catch(error => {
+        this.errorMessage = 'An error occurred while fetching the data.';
+        console.error('Error fetching absences:', error);
+      });
+  }
 }
